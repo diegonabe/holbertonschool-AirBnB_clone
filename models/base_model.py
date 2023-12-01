ba618e584fb05e842901dbbd0b42f425f30daac6
benjamin ontiveros <bontiveros15@gmail.com>
2023-12-01 14:55:06 -0800

    fixed: added new code kwargs and dictonaries

--- models/base_model.py
+++ models/base_model.py
@@ -10,15 +10,22 @@ class BaseModel:
     """
     Clase BaseModel para todas las subclases en el proyecto de AirBnb clone.
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Método constructor de la clase BaseModel
         """
-
-            self.id = str(uuid.uuid4())
+            time_format = "%Y-%m-%dT %H:%M:%S.%f"
+            if key == "__class__":
+                continue
+            elif key == "created_at" or key == "updated_at":
+                setattr(self, key, datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
+    else: 
+         self.id = str(uuid.uuid4())
         
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
+         self.created_at = datetime.utcnow()
+         self.updated_at = datetime.utcnow()
 
     def __str__(self):
         """
@@ -44,7 +51,7 @@ class BaseModel:
         return inst_dict
 
 
-if __name__ == "__main__":
+
 my_model = BaseModel()
 my_model.name = "My_First_Model"
 my_model.my_number = 89
@@ -57,3 +64,13 @@ print(my_model_json)
 print("JSON of my_model:")
 for key in my_model_json.keys():
     print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+
+print("--")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("--")
+print(my_model is my_new_model)
