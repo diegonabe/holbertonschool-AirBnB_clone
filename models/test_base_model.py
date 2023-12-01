69b958773e16085e12f96bb48e04957e49d669cd
benjamin ontiveros <bontiveros15@gmail.com>
2023-11-30 16:17:12 -0800

    fixed: add test_base_model.py

--- models/test_base_model.py
+++ models/test_base_model.py
@@ -1,48 +1,14 @@
 #!/usr/bin/python3
-"""
-Unittest para class BaseModel
-"""
-import unittest
 from models.base_model import BaseModel
 
-
-class TestBaseModel(unittest.TestCase):
-    def test_init(self):
-        my_model = BaseModel()
-
-        self.assertIsNotNone(my_model.id)
-        self.assertIsNotNone(my_model.created_at)
-        self.assertIsNotNone(my_model.updated_at)
-
-    def test_save(self):
-        my_model = BaseModel()
-
-        initial_updated_at = my_model.updated_at
-
-        current_updated_at = my_model.save()
-
-        self.assertNotEqual(initial_updated_at, current_updated_at)
-
-    def test_to_dict(self):
-        my_model = BaseModel()
-
-        my_model_dict = my_model.to_dict()
-
-        self.assertIsInstance(my_model_dict, dict)
-
-        self.assertEqual(my_model_dict["__class__"], 'BaseModel')
-        self.assertEqual(my_model_dict['id'], my_model.id)
-        self.assertEqual(my_model_dict['created_at'], my_model.created_at.isoformat())
-        self.assertEqual(my_model_dict['updated_at'], my_model.created_at.isoformat())
-
-    def test_str(self):
-        my_model = BaseModel()
-
-        self.assertTrue(str(my_model).startswith('[BaseModel]'))
-
-        self.assertIn(my_model.id, str(my_model))
-
-        self.assertIn(str(my_model.__dict__), str(my_model))
-
-if __name__ == "__main__":
-    unittest.main()
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
