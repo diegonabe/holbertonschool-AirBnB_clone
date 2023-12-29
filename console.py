5fc8043301c703d8b95ea732889438531a711a60
benjamin ontiveros <bontiveros15@gmail.com>
2023-12-29 12:10:35 -0800

    fixed: console

--- console.py
+++ console.py
@@ -3,7 +3,6 @@
 Console Module
 """
 import cmd
-import json
 import shlex
 from models import storage
 from models.base_model import BaseModel
@@ -30,59 +29,75 @@ class HBNBCommand(cmd.Cmd):
         """
         Prints the string representation of an instance based on the class name and id.
         """
-        if not args:
+        args_list = shlex.split(args)
+
+        if not args_list:
             print("** class name missing **")
             return
 
-        try:
-            args_list = shlex.split(args)
-            class_name = args_list[0]
-            instance_id = args_list[1]
-            key = "{}.{}".format(class_name, instance_id)
-            if key in storage.all():
-                print(storage.all()[key])
-            else:
-                print("** no instance found **")
-        except IndexError:
-            print("** instance id missing **")
-        except NameError:
+        class_name = args_list[0]
+
+        if class_name not in storage.classes():
             print("** class doesn't exist **")
+            return
+
+        if len(args_list) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_id = args_list[1]
+        key = "{}.{}".format(class_name, instance_id)
+
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+
+        print(storage.all()[key])
 
     def do_destroy(self, args):
         """
         Deletes an instance based on the class name and id.
         """
-        if not args:
+        args_list = shlex.split(args)
+
+        if not args_list:
             print("** class name missing **")
             return
 
-        try:
-            args_list = shlex.split(args)
-            class_name = args_list[0]
-            instance_id = args_list[1]
-            key = "{}.{}".format(class_name, instance_id)
-            if key in storage.all():
-                del storage.all()[key]
-                storage.save()
-            else:
-                print("** no instance found **")
-        except IndexError:
-            print("** instance id missing **")
-        except NameError:
+        class_name = args_list[0]
+
+        if class_name not in storage.classes():
             print("** class doesn't exist **")
+            return
+
+        if len(args_list) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_id = args_list[1]
+        key = "{}.{}".format(class_name, instance_id)
+
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+
+        del storage.all()[key]
+        storage.save()
 
     def do_all(self, args):
         """
         Prints all string representation of all instances based or not on the class name.
         """
+        args_list = shlex.split(args)
         objects = storage.all()
-        if args:
-            try:
-                args_list = shlex.split(args)
-                class_name = args_list[0]
-                objects = {k: v for k, v in objects.items() if k.startswith(class_name)}
-            except NameError:
-                print("** class doesn't exist **")
+
+        if args_list and args_list[0] not in storage.classes():
+            print("** class doesn't exist **")
+            return
+
+        if args_list:
+            class_name = args_list[0]
+            objects = {k: v for k, v in objects.items() if k.startswith(class_name)}
 
         print([str(obj) for obj in objects.values()])
 
@@ -90,53 +105,57 @@ class HBNBCommand(cmd.Cmd):
         """
         Updates an instance based on the class name and id by adding or updating attribute.
         """
-        if not args:
+        args_list = shlex.split(args)
+
+        if not args_list:
             print("** class name missing **")
             return
 
-        try:
-            args_list = shlex.split(args)
-            class_name = args_list[0]
-            instance_id = args_list[1]
-            key = "{}.{}".format(class_name, instance_id)
+        class_name = args_list[0]
 
-            if key not in storage.all():
-                print("** no instance found **")
-                return
-
-            if len(args_list) < 3:
-                print("** attribute name missing **")
-                return
-
-            attribute_name = args_list[2]
-
-            if len(args_list) < 4:
-                print("** value missing **")
-                return
-
-            value_str = args_list[3]
-            value = None
-
-            try:
-                value = int(value_str)
-            except ValueError:
-                try:
-                    value = float(value_str)
-                except ValueError:
-                    if value_str[0] == value_str[-1] == '"':
-                        value = value_str[1:-1]
-
-            if value is None:
-                print("** invalid value **")
-                return
-
-            setattr(storage.all()[key], attribute_name, value)
-            storage.all()[key].save()
-
-        except IndexError:
-            print("** instance id missing **")
-        except NameError:
+        if class_name not in storage.classes():
             print("** class doesn't exist **")
+            return
+
+        if len(args_list) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_id = args_list[1]
+        key = "{}.{}".format(class_name, instance_id)
+
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+
+        if len(args_list) < 3:
+            print("** attribute name missing **")
+            return
+
+        attribute_name = args_list[2]
+
+        if len(args_list) < 4:
+            print("** value missing **")
+            return
+
+        value_str = args_list[3]
+        value = None
+
+        try:
+            value = int(value_str)
+        except ValueError:
+            try:
+                value = float(value_str)
+            except ValueError:
+                if value_str[0] == value_str[-1] == '"':
+                    value = value_str[1:-1]
+
+        if value is None:
+            print("** invalid value **")
+            return
+
+        setattr(storage.all()[key], attribute_name, value)
+        storage.all()[key].save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
